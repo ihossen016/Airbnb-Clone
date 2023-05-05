211112b2152d12d64d554a6bd6b01162c9545aae
ihossen016 <ihossen016@gmail.com>
2023-05-05 11:44:42 +0600

    navbar created

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const Avatar = () => {
+    return (
+        <Image
+            alt="avatar"
+            src="/images/placeholder.jpg"
+            className="rounded-full"
+            height="30"
+            width="30"
+        />
+    );
+};
+
+export default Avatar;
