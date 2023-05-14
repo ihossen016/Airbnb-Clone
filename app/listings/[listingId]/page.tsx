1383c75de3a4873334a43ac052d3bf773633ec74
ihossen016 <ihossen016@gmail.com>
2023-05-14 22:04:32 +0600

    reservation enpoint created

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,7 +1,11 @@
+// actions
 import { getCurrentUser } from "@/app/actions/getCurrentUser";
 import getListingById from "@/app/actions/getListingById";
-import EmptyState from "@/app/components/EmptyState";
+import getReservations from "@/app/actions/getReservations";
+
+// components
 import ListingClient from "./ListingClient";
+import EmptyState from "@/app/components/EmptyState";
 
 interface IParams {
     listingId?: string;
@@ -9,13 +13,20 @@ interface IParams {
 
 const ListingPage = async ({ params }: { params: IParams }) => {
     const listing = await getListingById(params);
+    const reservations = await getReservations(params);
     const currentUser = await getCurrentUser();
 
     if (!listing) {
         return <EmptyState />;
     }
 
-    return <ListingClient listing={listing} currentUser={currentUser} />;
+    return (
+        <ListingClient
+            listing={listing}
+            reservations={reservations}
+            currentUser={currentUser}
+        />
+    );
 };
 
 export default ListingPage;
