6553ec11e1e01e7be9f501842af932b6b0e8c89b
ihossen016 <ihossen016@gmail.com>
2023-05-15 21:25:18 +0600

    properties page and listing delete function added

--- app/components/Navbar/UserMenu.tsx
+++ app/components/Navbar/UserMenu.tsx
@@ -80,7 +80,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                                 />
                                 <MenuItem
                                     label="My properties"
-                                    onClick={() => {}}
+                                    onClick={() => router.push("/properties")}
                                 />
                                 <MenuItem
                                     label="Airbnb my home"
